baabb558038e25bb428a0b7c6e4a5a427d636e84
Tim Kaechle <timothy.kaechle@me.com>
2023-08-22 15:16:41 -0300

    added basic search with google custom search, added connection to rethinkdb, fixed function calls

--- core/data/providers/news.py
+++ core/data/providers/news.py
@@ -1,13 +1,12 @@
 import requests
 
 
-def nytimes(topic: str = "world"):
-    api_key = "7NKUxGlG9nEuayaoQXaCuqAVdHqbDmYf"
-    url = f"https://api.nytimes.com/svc/topstories/v2/{topic}.json?api-key={api_key}"
-    response = requests.get(url)
+# def nytimes(topic: str = "world"):
+#     url = f"https://api.nytimes.com/svc/topstories/v2/{topic}.json?api-key={api_key}"
+#     response = requests.get(url)
 
-    if response.status_code == 200:
-        data = response.json()
-        print(data)
+#     if response.status_code == 200:
+#         data = response.json()
+#         print(data)
 
-    return response
+#     return response
