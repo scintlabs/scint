53c7179adfceac050903a3b8392a0b3b7fb8744c
Tim Kaechle <timothy.kaechle@me.com>
2023-08-31 12:09:23 -0300

    scaffolding for state management class

--- core/state.py
+++ core/state.py
@@ -8,28 +8,36 @@ APPDATA = os.path.join(envar("XDG_DATA_HOME"), APPNAME)
 
 
 class State:
+    PROCESSING = "processing"
+    GENERATING = "generating"
+    CHATTING = "chatting"
+    SEARCHING = "searching"
+    IDLE = "idle"
+
+    def __init__(self):
+        # Initial state is idle
+        self.current_state = self.IDLE
+
+    def set_state(self, new_state):
+        if new_state in [
+            self.PROCESSING,
+            self.GENERATING,
+            self.CHATTING,
+            self.SEARCHING,
+            self.IDLE,
+        ]:
+            self.current_state = new_state
+        else:
+            raise ValueError(f"Invalid state: {new_state}")
+
+    def get_state(self):
+        return self.current_state
+
+    def is_idle(self):
+        return self.current_state == self.IDLE
+
+    def __str__(self):
+        return f"AI Assistant is currently: {self.current_state}"
+
     def __init__(self):
         """Object for managing application state."""
-        logger.info(f"Initializing application state: idle.")
-        self.idle: bool = True
-
-    def process(self):
-        """State for reading and processing data."""
-        logger.info(f"State change: processor.")
-
-        if self.idle is True:
-            self.idle = False
-
-    def generate(self):
-        """State for generating text and code."""
-        logger.info(f"State change: generator.")
-        pass
-
-    def search(self):
-        """State for searching for information."""
-        pass
-
-    def chat(self):
-        """State for general discussion."""
-        logger.info(f"State change: chat.")
-        pass
