759b10fc2e28e1afd7bd4704b374be7c5626c807
Tim Kaechle <timothy.kaechle@me.com>
2023-11-30 19:04:51 -0300

    decoupled clients and workers from core backend

--- scint/app_setup.py
+++ scint/app_setup.py
@@ -1,11 +1,3 @@
 from core.persona import Persona
 
-from workers.web_browser import search_web, load_website
-from workers.api_parser import get_weather
-
 persona = Persona()
-persona.coordinator.add_workers(
-    search_web,
-    load_website,
-    get_weather,
-)
--- scint/handlers/events.py
+++
@@ -1,5 +0,0 @@
-from core.memory import Message
-
-
-def schedule_event():
-    return Message("system", "Event scheduled.", "schedule_event")
